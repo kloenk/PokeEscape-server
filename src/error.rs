ca56037b6af829edb24f74fa6c51d1a7409c0327
kloenk <kloenk@kloenk.de>
2019-04-04 12:59:19 +0200

    add ErrorKind to error

--- src/error.rs
+++ src/error.rs
@@ -1,40 +1,40 @@
 use std::fmt;
 use std::convert;
-use std::io::ErrorKind;
+use std::io;
 
 pub struct Error {
-    kind: String,
+    kind: ErrorKind,
 }
 
 impl Error {
     /// creates a new Error
-    pub fn new(kind: String) -> Error {
+    pub fn new(kind: ErrorKind) -> Error {
         Error {
             kind,
         }
     }
 
-    fn error_kind_as_str(kind: ErrorKind) -> String {
+    fn error_kind_as_str(kind: io::ErrorKind) -> ErrorKind {
         match kind {
-            ErrorKind::NotFound => String::from("entity not found"),
-            ErrorKind::PermissionDenied => String::from("permission denied"),
-            ErrorKind::ConnectionRefused => String::from("connection refused"),
-            ErrorKind::ConnectionReset => String::from("connection reset"),
-            ErrorKind::ConnectionAborted => String::from("connection aborted"),
-            ErrorKind::NotConnected => String::from("not connected"),
-            ErrorKind::AddrInUse => String::from("address in use"),
-            ErrorKind::AddrNotAvailable => String::from("address not available"),
-            ErrorKind::BrokenPipe => String::from("broken pipe"),
-            ErrorKind::AlreadyExists => String::from("entity already exists"),
-            ErrorKind::WouldBlock => String::from("operation would block"),
-            ErrorKind::InvalidInput => String::from("invalid input parameter"),
-            ErrorKind::InvalidData => String::from("invalid data"),
-            ErrorKind::TimedOut => String::from("timed out"),
-            ErrorKind::WriteZero => String::from("write zero"),
-            ErrorKind::Interrupted => String::from("operation interrupted"),
-            ErrorKind::Other => String::from("other os error"),
-            ErrorKind::UnexpectedEof => String::from("unexpected end of file"),
-            _ => String::from("unknown error"),
+            io::ErrorKind::NotFound => ErrorKind::io_NotFound,
+            io::ErrorKind::PermissionDenied => ErrorKind::io_PermissionDenied,
+            io::ErrorKind::ConnectionRefused => ErrorKind::io_ConnectionRefused,
+            io::ErrorKind::ConnectionReset => ErrorKind::io_ConnectionAborted,
+            io::ErrorKind::ConnectionAborted => ErrorKind::io_ConnectionAborted,
+            io::ErrorKind::NotConnected => ErrorKind::io_NotConnected,
+            io::ErrorKind::AddrInUse => ErrorKind::io_AddrInUse,
+            io::ErrorKind::AddrNotAvailable => ErrorKind::io_AddrNotAvailable,
+            io::ErrorKind::BrokenPipe => ErrorKind::io_BrokenPipe,
+            io::ErrorKind::AlreadyExists => ErrorKind::io_AlreadyExists,
+            io::ErrorKind::WouldBlock => ErrorKind::io_WouldBlock,
+            io::ErrorKind::InvalidInput => ErrorKind::io_InvalidInput,
+            io::ErrorKind::InvalidData => ErrorKind::io_InvalidData,
+            io::ErrorKind::TimedOut => ErrorKind::io_TimedOut,
+            io::ErrorKind::WriteZero => ErrorKind::io_writeZero,
+            io::ErrorKind::Interrupted => ErrorKind::io_Interrupted,
+            io::ErrorKind::Other => ErrorKind::io_Other,
+            io::ErrorKind::UnexpectedEof => ErrorKind::io_UnexpectedEof,
+            _ => ErrorKind::Unknown,
         }
     }
 }
@@ -43,7 +43,7 @@ impl Error {
 impl fmt::Display for Error {
     /// standart formater for print! macro
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Error of Kind {}", self.kind) // TODO: Colors
+        write!(f, "Error of Kind {}", self.kind.to_string()) // TODO: Colors
     }
 }
 
@@ -51,7 +51,7 @@ impl fmt::Display for Error {
 impl fmt::Debug for Error {
     /// formater for `Debug` in print! macro
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "(Error of Kind {})", self.kind)
+        write!(f, "(Error of Kind {})", self.kind.to_string())
     }
 }
 
@@ -67,7 +67,38 @@ impl std::convert::From<std::io::Error> for Error {
 impl std::convert::From<std::string::String> for Error {
     fn from(string: String) -> Self {
         Error {
-            kind: string,   // FIXME: better doing
+            kind: ErrorKind::Unknown,   // FIXME: better doing
+        }
+    }
+}
+
+pub enum ErrorKind {
+    io_NotFound,
+    io_PermissionDenied,
+    io_ConnectionRefused,
+    io_ConnectionReset,
+    io_ConnectionAborted,
+    io_NotConnected,
+    io_AddrInUse,
+    io_AddrNotAvailable,
+    io_BrokenPipe,
+    io_AlreadyExists,
+    io_WouldBlock,
+    io_InvalidInput,
+    io_InvalidData,
+    io_TimedOut,
+    io_writeZero,
+    io_Interrupted,
+    io_Other,
+    io_UnexpectedEof,
+    Unknown,
+}
+
+impl ErrorKind {
+    pub fn to_string(&self) -> String {
+        match self {
+            ErrorKind::io_NotFound => String::from("io_NotFound"),
+            _ => String::from("Unknown")
         }
     }
 }
