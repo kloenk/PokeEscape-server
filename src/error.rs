93b544624f632993f9a2b98fdb1a5ebde59c8cfd
kloenk <kloenk@kloenk.de>
2019-04-04 13:17:57 +0200

    rename errorKind fields

--- src/error.rs
+++ src/error.rs
@@ -16,25 +16,25 @@ impl Error {
 
     fn error_kind_as_str(kind: io::ErrorKind) -> ErrorKind {
         match kind {
-            io::ErrorKind::NotFound => ErrorKind::io_NotFound,
-            io::ErrorKind::PermissionDenied => ErrorKind::io_PermissionDenied,
-            io::ErrorKind::ConnectionRefused => ErrorKind::io_ConnectionRefused,
-            io::ErrorKind::ConnectionReset => ErrorKind::io_ConnectionAborted,
-            io::ErrorKind::ConnectionAborted => ErrorKind::io_ConnectionAborted,
-            io::ErrorKind::NotConnected => ErrorKind::io_NotConnected,
-            io::ErrorKind::AddrInUse => ErrorKind::io_AddrInUse,
-            io::ErrorKind::AddrNotAvailable => ErrorKind::io_AddrNotAvailable,
-            io::ErrorKind::BrokenPipe => ErrorKind::io_BrokenPipe,
-            io::ErrorKind::AlreadyExists => ErrorKind::io_AlreadyExists,
-            io::ErrorKind::WouldBlock => ErrorKind::io_WouldBlock,
-            io::ErrorKind::InvalidInput => ErrorKind::io_InvalidInput,
-            io::ErrorKind::InvalidData => ErrorKind::io_InvalidData,
-            io::ErrorKind::TimedOut => ErrorKind::io_TimedOut,
-            io::ErrorKind::WriteZero => ErrorKind::io_writeZero,
-            io::ErrorKind::Interrupted => ErrorKind::io_Interrupted,
-            io::ErrorKind::Other => ErrorKind::io_Other,
-            io::ErrorKind::UnexpectedEof => ErrorKind::io_UnexpectedEof,
-            _ => ErrorKind::Unknown,
+            io::ErrorKind::NotFound => ErrorKind::IoNotFound,
+            io::ErrorKind::PermissionDenied => ErrorKind::IoPermissionDenied,
+            io::ErrorKind::ConnectionRefused => ErrorKind::IoConnectionRefused,
+            io::ErrorKind::ConnectionReset => ErrorKind::IoConnectionAborted,
+            io::ErrorKind::ConnectionAborted => ErrorKind::IoConnectionAborted,
+            io::ErrorKind::NotConnected => ErrorKind::IoNotConnected,
+            io::ErrorKind::AddrInUse => ErrorKind::IoAddrInUse,
+            io::ErrorKind::AddrNotAvailable => ErrorKind::IoAddrNotAvailable,
+            io::ErrorKind::BrokenPipe => ErrorKind::IoBrokenPipe,
+            io::ErrorKind::AlreadyExists => ErrorKind::IoAlreadyExists,
+            io::ErrorKind::WouldBlock => ErrorKind::IoWouldBlock,
+            io::ErrorKind::InvalidInput => ErrorKind::IoInvalidInput,
+            io::ErrorKind::InvalidData => ErrorKind::IoInvalidData,
+            io::ErrorKind::TimedOut => ErrorKind::IoTimedOut,
+            io::ErrorKind::WriteZero => ErrorKind::IowriteZero,
+            io::ErrorKind::Interrupted => ErrorKind::IoInterrupted,
+            io::ErrorKind::Other => ErrorKind::IoOther,
+            io::ErrorKind::UnexpectedEof => ErrorKind::IoUnexpectedEof,
+            _ => ErrorKind::Unknown(String::from("uknwon io errord")),
         }
     }
 }
@@ -67,37 +67,37 @@ impl std::convert::From<std::io::Error> for Error {
 impl std::convert::From<std::string::String> for Error {
     fn from(string: String) -> Self {
         Error {
-            kind: ErrorKind::Unknown,   // FIXME: better doing
+            kind: ErrorKind::Unknown(string),   // FIXME: better doing
         }
     }
 }
 
 pub enum ErrorKind {
-    io_NotFound,
-    io_PermissionDenied,
-    io_ConnectionRefused,
-    io_ConnectionReset,
-    io_ConnectionAborted,
-    io_NotConnected,
-    io_AddrInUse,
-    io_AddrNotAvailable,
-    io_BrokenPipe,
-    io_AlreadyExists,
-    io_WouldBlock,
-    io_InvalidInput,
-    io_InvalidData,
-    io_TimedOut,
-    io_writeZero,
-    io_Interrupted,
-    io_Other,
-    io_UnexpectedEof,
-    Unknown,
+    IoNotFound,
+    IoPermissionDenied,
+    IoConnectionRefused,
+    IoConnectionReset,
+    IoConnectionAborted,
+    IoNotConnected,
+    IoAddrInUse,
+    IoAddrNotAvailable,
+    IoBrokenPipe,
+    IoAlreadyExists,
+    IoWouldBlock,
+    IoInvalidInput,
+    IoInvalidData,
+    IoTimedOut,
+    IowriteZero,
+    IoInterrupted,
+    IoOther,
+    IoUnexpectedEof,
+    Unknown(String),
 }
 
 impl ErrorKind {
     pub fn to_string(&self) -> String {
         match self {
-            ErrorKind::io_NotFound => String::from("io_NotFound"),
+            ErrorKind::IoNotFound => String::from("io_NotFound"),
             _ => String::from("Unknown")
         }
     }
