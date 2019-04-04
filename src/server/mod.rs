0ce381238de2336006450b2b4c24981372e25eb0
kloenk <kloenk@kloenk.de>
2019-04-04 09:23:28 +0200

    remove code that can panic from server::negotiate

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -8,33 +8,48 @@ pub mod http;
 
 /// This function negotiates the protocoll to use between the client and the Server
 /// it calles the function of the protocoll, uses &TcpStream and a buffer as arguments
-/// 
-/// # panics
-/// has many unwrap functions
 pub fn negotiate(mut conf: Job) -> Result<(), String>{  //TODO: add return types
     let mut reader = match conf.stream.try_clone() {
       Ok(stream) => BufReader::new(stream),
-      Err(err) => return Err(err.to_string()),
+      Err(err) => return Err(err.to_string()),  // return type
     };
     let mut line = String::new();
-    reader.read_line(&mut line).unwrap();
+    match reader.read_line(&mut line) {
+        Ok(_) => (),    // would return read bytes
+        Err(err) => return Err(err.to_string()),        // return type
+    };
     if conf.verbose {
-        let addr = conf.stream.peer_addr().unwrap().to_string();
+        let addr = match conf.stream.peer_addr() {
+            Ok(addr) => addr.to_string(),
+            Err(err) => return Err(err.to_string()),    // return type
+        };
         println!("got {} from {}", line.trim().yellow(), addr.green());
     }
 
     if line.starts_with("POKEMON-ESCAPE_") {
         // run pokemon server
         eprintln!("fix POKEMON-ESCAPE-CLIENT");
-        conf.stream
-            .write(format!("POKEMON-ESCAPE-SERVER_{}\n", env!("CARGO_PKG_VERSION")).as_bytes()).unwrap();
+        match conf.stream
+            .write(format!("POKEMON-ESCAPE-SERVER_{}\n", env!("CARGO_PKG_VERSION")).as_bytes()) {
+                Ok(_) => (),    // would return bytes written
+                Err(err) => return Err(err.to_string()),    // return type
+            };
     } else if line.contains("HTTP/1.1") {
-        http::handle_client(&mut conf.stream, reader).unwrap();
+        match http::handle_client(&mut conf.stream, reader) {
+            Ok(stream) => stream,
+            Err(err) => return Err(err.to_string()),    // return type
+        };
     } else {
-        conf.stream.write(b"Protocol mismatch.").unwrap();  //FIXME: unwrap
+        match conf.stream.write(b"Protocol mismatch.") {
+            Ok(_) => (),    // would return bytes written
+            Err(err) => return Err(err.to_string()),    //return type
+        };  //FIXME: unwrap
     }
-    conf.stream.flush().unwrap();
-    Ok(())
+    match conf.stream.flush() {
+        Ok(_) => (),
+        Err(err) => return Err(err.to_string()),    // return type
+    };
+    Ok(())  // return type
 }
 
 /// starts the connection to the client
