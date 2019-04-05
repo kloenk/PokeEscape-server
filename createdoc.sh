666561e395cb836f55e17f1cdcae3f95a24bd69c
kloenk <kloenk@kloenk.de>
2019-04-05 21:33:15 +0200

    index.html doc foo

--- createdoc.sh
+++ createdoc.sh
@@ -2,7 +2,16 @@
 
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  if [ "$TRAVIS_RUST_VERSION" == "stable"]; then
+  if [ "$TRAVIS_RUST_VERSION" == "stable" ]; then
     cargo doc --target-dir public
+    cat <<EOF > ./public/index.html 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" lang="en"/>
+        <meta http-equiv="refresh" content="0; URL='/pokemon_escape_server/doc/pokemon_escape_server/'" />
+    </head>
+</html>
+EOF
   fi
 fi
