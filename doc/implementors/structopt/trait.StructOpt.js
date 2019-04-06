8a395cd4116b38704b5e25cfaae1a5f23c7ce91f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-06 15:31:11 +0000

    Deploy Kloenk/pokemon_escape_server to github.com/Kloenk/pokemon_escape_server.git:gh-pages

--- doc/implementors/structopt/trait.StructOpt.js
+++ doc/implementors/structopt/trait.StructOpt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["pokemon_escape_server"] = [{text:"impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"pokemon_escape_server/struct.Config.html\" title=\"struct pokemon_escape_server::Config\">Config</a>",synthetic:false,types:["pokemon_escape_server::Config"]},];
+implementors["poke_escape_server"] = [{text:"impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"poke_escape_server/struct.Config.html\" title=\"struct poke_escape_server::Config\">Config</a>",synthetic:false,types:["poke_escape_server::Config"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
