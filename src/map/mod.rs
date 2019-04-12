440356aec199be7dae4a02f39e64a2d0c0eef718
Kloenk <kloenk@kloenk.de>
2019-04-12 17:09:14 +0200

    add first version of maploader
    will no convert from vec to hashMap

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -22,76 +22,126 @@ impl MapPlaces {
 
         let file = fs::read_to_string(file)?;
         let content: Value = toml::from_str(file.as_str())?;
-        let maps = Map::from_conf(&content, &String::from("test"), verbose)?;
         let version = match content["Maps"]["version"].as_str() {
             Some(ver) => ver,
             None => return Err(Error::new(super::error::ErrorKind::FieldNotExists)),
         };
         let version = Version::from_str(version)?;
+
+        let mut maps = Vec::new();
+
+        if version < Version::new(99, 99, 99) {
+            // check in reserve order for version
+            maps = Map::from_conf(&content, verbose)?;
+        }
         Ok(MapPlaces {
             p_version: version,
-            p_maps: vec![maps],
+            p_maps: maps,
         })
     }
 }
 
 /// this holds a single map with all of the coresponding informations
+///
+/// This function willnot preload the map, but loads it when used (random??)
 struct Map {
-    // TODO: public?
-    name: String,
-    map: String, // json::Value
-    version: Version,
+    /// name of the given map
+    p_name: String,
+
+    /// file to load the map from
+    p_file: String,
+
+    /// version of the maploader to use
+    p_version: Version,
+
+    /// format of the given map file
+    p_format: MapFormat,
+
+    /// set to true if the mapload should operate in verbose mode
+    p_verbose: bool,
 }
 
 impl Map {
+    /// creates a new instance of the Map
+    /// This function only returns the struct with the given data filled in, so there is no logic involved
     pub fn new(
-        content: String, /*json::Value*/
         name: String,
+        file: String,
         version: Version,
-    ) -> Result<Self, Error> {
-        //TODO: add version checking (content == Version)
-
-        // parse version
-        Ok(Map {
-            name,
-            version,
-            map: content,
-        })
+        format: MapFormat,
+        verbose: bool,
+    ) -> Self {
+        Map {
+            p_name: name,
+            p_file: file,
+            p_version: version,
+            p_format: format,
+            p_verbose: verbose,
+        }
     }
 
-    pub fn from_conf(toml: &toml::Value, name: &String, verbose: bool) -> Result<Self, Error> {
-        //FIXME: test if map exists
-        if toml[name].is_table() {
+    pub fn from_conf(toml: &toml::Value, verbose: bool) -> Result<Vec<Self>, Error> {
+        if !toml["Maps"]["maps"].is_array() {
             return Err(Error::new_field_not_exists());
         }
 
-        let version = match toml[name]["version"].as_str() {
-            Some(ver) => ver,
+        let maps_names = match toml["Maps"]["maps"].as_array() {
+            Some(maps) => maps,
             None => return Err(Error::new_field_not_exists()),
         };
-        let version = Version::from_str(version)?; // parse version from toml to version
 
-        let file = match toml[name]["path"].as_str() {
-            Some(file) => file,
-            None => return Err(Error::new_field_not_exists()),
-        };
-        let file = fs::read_to_string(file)?; // load map into ram
+        let mut maps = Vec::new();
 
-        let format = match toml[name]["format"].as_str() {
+        for map in maps_names {
+            let map = match map.as_str() {
+                Some(map) => map,
+                None => {
+                    eprintln!("Critical error reading a map!");
+                    "err"
+                } // FIXME: better error handling
+            };
+            if !toml[map].is_table() {
+                eprintln!("Map {} not found in config", map);
+            }
+
+            maps.push(Self::from_conf_one(&toml[map], map.to_string(), verbose)?);
+        }
+
+        Ok(maps)
+    }
+
+    fn from_conf_one(toml: &toml::Value, name: String, verbose: bool) -> Result<Self, Error> {
+        let file: String = unwrap_some(toml["path"].as_str())?.to_string();
+        let version = unwrap_some(toml["version"].as_str())?;
+        let version = Version::from_str(version)?;
+
+        let format_str = match toml["format"].as_str() {
             Some(format) => format,
-            None => "json",
+            None => "JSON", // defaults to json
         };
-        if format.to_lowercase() == "json" {
-            // parse from json
+
+        let mut format: MapFormat;
+
+        if format_str.to_lowercase() == "json" {
+            format = MapFormat::JSON;
         } else {
             return Err(Error::new(super::error::ErrorKind::FormatNotSupported));
         }
 
-        Self::new(
-            // already answers with Result
-            file,         // the map itself
-            name.clone(), // name of the map
-            version,      // version of map
-        )
+        Ok(Self::new(name, file, version, format, verbose))
     }
+}
+
+/// unwrap_some unwraps a some value and returns a Result<T, Error>
+fn unwrap_some<T>(value: Option<T>) -> Result<T, Error> {
+    match value {
+        Some(value) => Ok(value),
+        None => Err(Error::new_field_not_exists()),
+    }
+}
+
+/// map format is a enum of possible loader elements for a map file
+enum MapFormat {
+    /// JSON as the javascript object notation
+    JSON,
 }
