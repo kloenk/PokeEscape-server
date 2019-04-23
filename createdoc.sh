a7d72b2bb0fcf526f9f48f6d0d9db574e9dff355
Kloenk <kloenk@kloenk.de>
2019-04-23 21:49:22 +0200

    add private field to doc

--- createdoc.sh
+++ createdoc.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   if [ "$TRAVIS_RUST_VERSION" == "stable" ]; then
-    cargo doc --target-dir public
+    cargo doc --document-private-items --target-dir public
     cat <<EOF > ./public/index.html 
 <!DOCTYPE html>
 <html>
